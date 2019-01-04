d3a3f9a2959d245220e978b06a6316049e17f4cd
lumozx <969671691@qq.com>
2019-01-04 17:14:52 +0800

    前端文件修改

--- config/index.js
+++ config/index.js
@@ -47,7 +47,7 @@ module.exports = {
     authServer: 'https://www.workbei.com/backauth/',
     // authServer: 'http://dd.rsq.etoutiao.cn/rsqauth/',
     // 日事清前端文件地址
-    frontServer: 'https://rishiqing-front.oss-cn-beijing.aliyuncs.com/dingtalk/workbei-pc/',
+    frontServer: 'https://res-front-cdn.timetask.cn/dingtalk/workbei-pc/',
     // 阿里云OSS STS认证的server地址，临时！！！！注意更新
     stsServer: 'https://stsserver.hz.taeapp.com/sts/',
     // stsServer: 'http://182.92.222.40:8300/sts/',
